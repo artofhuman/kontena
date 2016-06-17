f2e3b4bafd9dc497d50903201e36bf7cffd957bb
Jussi Nummelin <jussi.nummelin@kontena.io>
2016-06-17 15:47:04 +0300

    Initial health check for HTTP services
    added tcp check capability
    
    added http check for lb config in etcd
    
    code cleanup

--- cli/lib/kontena/cli/services/services_helper.rb
+++ cli/lib/kontena/cli/services/services_helper.rb
@@ -170,6 +170,22 @@ module Kontena
             puts "  pid: #{service['pid']}"
           end
 
+          if service['health_check']
+            puts "  health check:"
+            puts "    protocol: #{service['health_check']['protocol']}"
+            puts "    uri: #{service['health_check']['uri']}" if service['health_check']['protocol'] == 'http'
+            puts "    port: #{service['health_check']['port']}"
+            puts "    timeout: #{service['health_check']['timeout']}"
+            puts "    interval: #{service['health_check']['interval']}"
+            puts "    initial_delay: #{service['health_check']['initial_delay']}"
+          end
+          
+          if service['health_status']
+            puts "  health status:"
+            puts "    healthy: #{service['health_status']['healthy']}"
+            puts "    total: #{service['health_status']['total']}"
+          end
+
           puts "  instances:"
           result = client(token).get("services/#{parse_service_id(service_id)}/containers")
           result['containers'].each do |container|
@@ -362,6 +378,7 @@ module Kontena
             'TB' => 1000 * 1000 * 1000 * 1000 * 1000
           }.each_pair { |e, s| return "#{(int.to_i / (s / 1000))}#{e}" if int < s }
         end
+
       end
     end
   end
